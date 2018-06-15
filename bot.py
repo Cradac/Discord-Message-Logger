b5d16f08dc039651dfd99cf80723372142d86941
Cradac <39528837+Cradac@users.noreply.github.com>
2018-06-15 09:35:17 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,6 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
-import re
-
 
 Client = discord.Client()
 client = commands.Bot(command_prefix = "?")
